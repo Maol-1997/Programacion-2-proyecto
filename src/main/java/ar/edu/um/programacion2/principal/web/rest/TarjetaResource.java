5782c39313cedc26352a47ce85ae136aa2c9a7c5
Maol <m.ortiz@alumno.um.edu.ar>
2019-10-06 00:01:28 -0300

    crear tarjeta de credito

--- src/main/java/ar/edu/um/programacion2/principal/web/rest/TarjetaResource.java
+++ src/main/java/ar/edu/um/programacion2/principal/web/rest/TarjetaResource.java
@@ -7,25 +7,31 @@ import ar.edu.um.programacion2.principal.repository.UserRepository;
 import ar.edu.um.programacion2.principal.security.AuthoritiesConstants;
 import ar.edu.um.programacion2.principal.security.SecurityUtils;
 import ar.edu.um.programacion2.principal.web.rest.errors.BadRequestAlertException;
-
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.PaginationUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
-
+import java.net.URL;
 import java.util.List;
 import java.util.Optional;
 
@@ -67,13 +73,72 @@ public class TarjetaResource {
             throw new BadRequestAlertException("A new tarjeta cannot already have an ID", ENTITY_NAME, "idexists");
         }
         if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
-        if(clienteRepository.findById(tarjeta.getCliente().getId()).get().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId() || tarjeta.getCliente() == null)//seguridad
-            return ResponseEntity.badRequest().build();
+            if (clienteRepository.findById(tarjeta.getCliente().getId()).get().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId() || tarjeta.getCliente() == null)//seguridad
+                return ResponseEntity.badRequest().build();
 
         Tarjeta result = tarjetaRepository.save(tarjeta);
         return ResponseEntity.created(new URI("/api/tarjetas/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
             .body(result);
+    }
+
+    @PostMapping("/tarjetas/añadir")
+    public ResponseEntity<Tarjeta> añadirTarjeta(@Valid @RequestBody JSONObject json) throws JSONException, IOException, URISyntaxException {
+        log.debug("REST request to add Tarjeta : {}", json);
+
+        if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+            if (clienteRepository.findById(Long.valueOf(json.getString("cliente_id"))).get().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId()) // seguridad
+                return ResponseEntity.badRequest().build();
+            if (json.getString("nombre") == null || json.getString("apellido") == null || json.getString("vencimiento") == null || json.getString("numero") == null || json.getString("seguridad") == null || json.getString("cliente_id") == null)
+                return ResponseEntity.badRequest().build();
+        }
+
+        String ult4 = json.getString("numero").substring(json.getString("numero").length() - 4);
+        String jsonInputString =
+                "{\"nombre\": \"" + json.getString("nombre") + "\"," +
+                "\"apellido\": \"" + json.getString("apellido") + "\"," +
+                "\"vencimiento\": \"" + json.getString("vencimiento") + "\"," +
+                "\"numero\": \"" + json.getString("numero") + "\"," +
+                "\"seguridad\": \"" + json.getString("seguridad") + "\"}";
+
+        String token = sendPostTarjeta(jsonInputString);
+        Tarjeta tarjeta = new Tarjeta();
+        tarjeta.setToken(token);
+        tarjeta.setAlta(true);
+        tarjeta.setUltDigitos(Integer.valueOf(ult4));
+        tarjeta.setCliente(clienteRepository.findById(Long.valueOf(json.getString("cliente_id"))).get());
+        Tarjeta result = tarjetaRepository.save(tarjeta);
+
+        return ResponseEntity.created(new URI("/api/tarjetas/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    private String sendPostTarjeta(String json) throws IOException {
+        String url;
+        url = "http://localhost:8081/tarjeta/creartoken";
+        System.out.println(url);
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setDoOutput(true);
+
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = json.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        } // envia el json
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            return response.toString();
+        }
     }
 
     /**
@@ -92,11 +157,11 @@ public class TarjetaResource {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
         }
 
-        if(!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
-            if(clienteRepository.findById(tarjeta.getCliente().getId()).get().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId() || tarjeta.getCliente() == null) //seguridad
+        if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
+            if (clienteRepository.findById(tarjeta.getCliente().getId()).get().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId() || tarjeta.getCliente() == null) //seguridad
                 throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "forbidden");
-            if(tarjeta.getCliente() == null)
-                throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "null cliente");
+        if (tarjeta.getCliente() == null)
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "null cliente");
 
         Tarjeta result = tarjetaRepository.save(tarjeta);
         return ResponseEntity.ok()
@@ -107,9 +172,7 @@ public class TarjetaResource {
     /**
      * {@code GET  /tarjetas} : get all the tarjetas.
      *
-
      * @param pageable the pagination information.
-
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of tarjetas in body.
      */
     @GetMapping("/tarjetas")
@@ -119,7 +182,7 @@ public class TarjetaResource {
             Page<Tarjeta> page = tarjetaRepository.findAll(pageable);
             HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
             return ResponseEntity.ok().headers(headers).body(page.getContent());
-        }else{
+        } else {
             List<Tarjeta> list = tarjetaRepository.findByUserIsCurrentUser();
             Page<Tarjeta> page = new PageImpl<>(list);
             HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
@@ -136,8 +199,8 @@ public class TarjetaResource {
     @GetMapping("/tarjetas/{id}")
     public ResponseEntity<Tarjeta> getTarjeta(@PathVariable Long id) {
         log.debug("REST request to get Tarjeta : {}", id);
-        if(!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
-            if(tarjetaRepository.findById(id).get().getCliente().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId()) //seguridad
+        if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
+            if (tarjetaRepository.findById(id).get().getCliente().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId()) //seguridad
                 throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "forbidden");
         Optional<Tarjeta> tarjeta = tarjetaRepository.findById(id);
         return ResponseUtil.wrapOrNotFound(tarjeta);
@@ -152,8 +215,8 @@ public class TarjetaResource {
     @DeleteMapping("/tarjetas/{id}")
     public ResponseEntity<Void> deleteTarjeta(@PathVariable Long id) {
         log.debug("REST request to delete Tarjeta : {}", id);
-        if(!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
-            if(tarjetaRepository.findById(id).get().getCliente().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId()) //seguridad
+        if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
+            if (tarjetaRepository.findById(id).get().getCliente().getUser().getId() != userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId()) //seguridad
                 throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "forbidden");
         tarjetaRepository.deleteById(id);
         return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
