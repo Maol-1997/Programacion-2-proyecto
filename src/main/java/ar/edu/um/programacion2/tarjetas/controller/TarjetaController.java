e909c97bcf3f6a86de5b13eacc95c41ac3d747e2
Maol <m.ortiz@alumno.um.edu.ar>
2019-10-12 16:33:51 -0300

    estructura arreglada

--- src/main/java/ar/edu/um/programacion2/tarjetas/controller/TarjetaController.java
+++ src/main/java/ar/edu/um/programacion2/tarjetas/controller/TarjetaController.java
@@ -7,17 +7,12 @@ import ar.edu.um.programacion2.tarjetas.model.Tarjeta;
 import ar.edu.um.programacion2.tarjetas.model.TarjetaAddDTO;
 import ar.edu.um.programacion2.tarjetas.model.TarjetaDTO;
 import ar.edu.um.programacion2.tarjetas.service.TarjetaService;
-import com.google.common.hash.Hashing;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Random;
 
 
 @RestController
@@ -37,33 +32,12 @@ public class TarjetaController {
 	}
 	@PostMapping("/")
 	public ResponseEntity<String> add(@RequestBody TarjetaAddDTO tarjetaAddDTO) {
-		String token = tarjetaAddDTO.getNombre() + tarjetaAddDTO.getApellido() + tarjetaAddDTO.getVencimiento() + tarjetaAddDTO.getNumero() + tarjetaAddDTO.getSeguridad() + System.currentTimeMillis();
-		token = Hashing.sha256().hashString(token, StandardCharsets.UTF_8).toString();
-		Tarjeta tarjeta = new Tarjeta();
-		tarjeta.setLimite(tarjetaAddDTO.getLimite());
-		tarjeta.setToken(token);
-		tarjeta.setExpira(tarjetaAddDTO.getVencimiento());
-		service.add(tarjeta);
-		return new ResponseEntity<String>(token,HttpStatus.OK);
+		return new ResponseEntity<String>(service.crear(tarjetaAddDTO),HttpStatus.OK);
 	}
 
 	@PostMapping("/comprar")
 	public ResponseEntity<String> comprar(@RequestBody TarjetaDTO tarjetaDTO) {
-		Tarjeta tarjeta = service.findByToken(tarjetaDTO.getToken());
-		if(tarjeta == null)
-			return new ResponseEntity<String>("Error, la tarjeta no existe en la DB",HttpStatus.NOT_FOUND);
-		if(tarjeta.getLimite() < Integer.valueOf(tarjetaDTO.getMonto()))
-			return new ResponseEntity<String>("La compra excede el limite de la tarjeta",HttpStatus.UNAUTHORIZED);
-
-		LocalDate today = LocalDate.now();
-		String [] expira = tarjeta.getExpira().split("/");
-		if((today.getMonthValue() > Integer.valueOf(expira[0]) && today.getYear() >= Integer.valueOf(expira[1])))
-			return new ResponseEntity<String>("Tarjeta expirada",HttpStatus.UNAUTHORIZED);
-
-		Random r = new Random();
-		if((r.nextInt((10 - 1) + 1) + 1) == 5 || (r.nextInt((10 - 1) + 1) + 1) == 6) //20% chances de saldo insuficiente
-			return new ResponseEntity<String>("Saldo insuficiente",HttpStatus.UNAUTHORIZED);
-		return new ResponseEntity<String>("Operacion realizada con exito",HttpStatus.OK);
+		return new ResponseEntity<String>(service.comprar(tarjetaDTO),HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{tarjetaId}")
@@ -73,11 +47,6 @@ public class TarjetaController {
 
 	@PutMapping("{tarjetaId}")
 	public ResponseEntity<Tarjeta> update(@RequestBody TarjetaAddDTO tarjetaAddDTO, @PathVariable Long tarjetaId) {
-        String token = tarjetaAddDTO.getNombre() + tarjetaAddDTO.getApellido() + tarjetaAddDTO.getVencimiento() + tarjetaAddDTO.getNumero() + tarjetaAddDTO.getSeguridad() + System.currentTimeMillis();
-        token = Hashing.sha256().hashString(token, StandardCharsets.UTF_8).toString();
-		Tarjeta tarjeta = new Tarjeta();
-		tarjeta.setLimite(tarjetaAddDTO.getLimite());
-		tarjeta.setToken(token);
-		return new ResponseEntity<Tarjeta>(service.update(tarjeta, tarjetaId), HttpStatus.OK);
+		return new ResponseEntity<Tarjeta>(service.actualizar(tarjetaAddDTO, tarjetaId), HttpStatus.OK);
 	}
 }
--- src/main/java/ar/edu/um/programacion2/tarjetas/service/TarjetaService.java
+++ src/main/java/ar/edu/um/programacion2/tarjetas/service/TarjetaService.java
@@ -6,10 +6,16 @@ package ar.edu.um.programacion2.tarjetas.service;
 import ar.edu.um.programacion2.tarjetas.Repository.ITarjetaRepository;
 import ar.edu.um.programacion2.tarjetas.exceptions.TarjetaNotFoundException;
 import ar.edu.um.programacion2.tarjetas.model.Tarjeta;
+import ar.edu.um.programacion2.tarjetas.model.TarjetaAddDTO;
+import ar.edu.um.programacion2.tarjetas.model.TarjetaDTO;
+import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author daniel
@@ -27,6 +33,45 @@ public class TarjetaService {
     public Tarjeta add(Tarjeta tarjeta) {
         repository.save(tarjeta);
         return tarjeta;
+    }
+
+    public String crear(TarjetaAddDTO tarjetaAddDTO){
+        String token = tarjetaAddDTO.getNombre() + tarjetaAddDTO.getApellido() + tarjetaAddDTO.getVencimiento() + tarjetaAddDTO.getNumero() + tarjetaAddDTO.getSeguridad() + System.currentTimeMillis();
+        token = Hashing.sha256().hashString(token, StandardCharsets.UTF_8).toString();
+        Tarjeta tarjeta = new Tarjeta();
+        tarjeta.setLimite(tarjetaAddDTO.getLimite());
+        tarjeta.setToken(token);
+        tarjeta.setExpira(tarjetaAddDTO.getVencimiento());
+        add(tarjeta);
+        return token;
+    }
+
+    public String comprar(TarjetaDTO tarjetaDTO){
+        Tarjeta tarjeta = findByToken(tarjetaDTO.getToken());
+        if(tarjeta == null)
+            return "Error, la tarjeta no existe en la DB";
+        if(tarjeta.getLimite() < Integer.valueOf(tarjetaDTO.getMonto()))
+            return "La compra excede el limite de la tarjeta";
+
+        LocalDate today = LocalDate.now();
+        String [] expira = tarjeta.getExpira().split("/");
+        if((today.getMonthValue() > Integer.valueOf(expira[0]) && today.getYear() >= Integer.valueOf(expira[1])))
+            return "Tarjeta expirada";
+
+        Random r = new Random();
+        if((r.nextInt((10 - 1) + 1) + 1) == 5 || (r.nextInt((10 - 1) + 1) + 1) == 6) //20% chances de saldo insuficiente
+            return "Saldo insuficiente";
+        return "Operacion realizada con exito";
+    }
+
+    public Tarjeta actualizar(TarjetaAddDTO tarjetaAddDTO,long tarjetaId){
+        String token = tarjetaAddDTO.getNombre() + tarjetaAddDTO.getApellido() + tarjetaAddDTO.getVencimiento() + tarjetaAddDTO.getNumero() + tarjetaAddDTO.getSeguridad() + System.currentTimeMillis();
+        token = Hashing.sha256().hashString(token, StandardCharsets.UTF_8).toString();
+        Tarjeta tarjeta = new Tarjeta();
+        tarjeta.setLimite(tarjetaAddDTO.getLimite());
+        tarjeta.setToken(token);
+        Tarjeta newTarjeta = update(tarjeta, tarjetaId);
+        return newTarjeta;
     }
 
 
