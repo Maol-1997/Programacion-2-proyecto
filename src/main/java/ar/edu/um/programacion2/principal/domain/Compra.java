4749a58b7c4f6b73f36f3724c764d6946da87c8a
Jeronimo <jerotaber@gmail.com>
2019-12-09 12:10:38 -0300

    domain compra

---
+++ src/main/java/ar/edu/um/programacion2/principal/domain/Compra.java
@@ -0,0 +1,84 @@
+package ar.edu.um.programacion2.principal.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "compra")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Compra {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Column(name = "token", nullable = false)
+	private String token;
+	
+	@NotNull
+	@Column(name = "token", nullable = false)
+	private Float precio;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("compra")
+	private Cliente cliente;
+
+	public Compra() {
+		super();
+	}
+
+	public Compra(Long id, @NotNull String token, @NotNull Float precio, Cliente cliente) {
+		super();
+		this.id = id;
+		this.token = token;
+		this.precio = precio;
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+}
