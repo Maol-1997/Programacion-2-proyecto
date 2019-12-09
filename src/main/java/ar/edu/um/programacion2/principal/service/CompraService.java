fb74c4344c56c11c00fa76e57db946a5e49fb59a
Jeronimo <jerotaber@gmail.com>
2019-12-09 13:35:14 -0300

    lo que faltaba de compra

--- src/main/java/ar/edu/um/programacion2/principal/service/CompraService.java
+++ src/main/java/ar/edu/um/programacion2/principal/service/CompraService.java
@@ -1,11 +1,12 @@
 package ar.edu.um.programacion2.principal.service;
 
+import ar.edu.um.programacion2.principal.domain.Compra;
 import ar.edu.um.programacion2.principal.repository.CompraRepository;
 
 public class CompraService {
     private CompraRepository compraRepository;
 
-    public Compra crearCompra () {
-    	
+    public Compra crearCompra() {
+    	return null;
     }
 }
