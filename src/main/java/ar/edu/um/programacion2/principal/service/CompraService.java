58d5fc488118953eb9562456d9cbbf3634599c78
Jeronimo <jerotaber@gmail.com>
2019-12-14 17:59:56 -0300

    Re Change CompraService

--- src/main/java/ar/edu/um/programacion2/principal/service/CompraService.java
+++ src/main/java/ar/edu/um/programacion2/principal/service/CompraService.java
@@ -1,5 +1,6 @@
 package ar.edu.um.programacion2.principal.service;
 
+import ar.edu.um.programacion2.principal.domain.Cliente;
 import ar.edu.um.programacion2.principal.domain.Compra;
 import ar.edu.um.programacion2.principal.repository.ClienteRepository;
 import ar.edu.um.programacion2.principal.repository.CompraRepository;
@@ -8,12 +9,17 @@ import ar.edu.um.programacion2.principal.repository.UserRepository;
 import ar.edu.um.programacion2.principal.security.AuthoritiesConstants;
 import ar.edu.um.programacion2.principal.security.SecurityUtils;
 import ar.edu.um.programacion2.principal.service.dto.CompraDTO;
+import ar.edu.um.programacion2.principal.service.dto.LogDTO;
 import ar.edu.um.programacion2.principal.service.dto.TarjetaDTO;
 import ar.edu.um.programacion2.principal.service.util.PostUtil;
 import ar.edu.um.programacion2.principal.web.rest.errors.BadRequestAlertException;
+
+import com.netflix.ribbon.proxy.annotation.Http;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +27,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service Interface for managing
@@ -29,61 +36,34 @@ import java.util.List;
 
 @Service
 public class CompraService {
-    @Autowired
-    private final CompraRepository compraRepository;
-    @Autowired
-    private final TarjetaRepository tarjetaRepository;
-    @Autowired
-    private final ClienteRepository clienteRepository;
-    @Autowired
-    private final UserRepository userRepository;
+	@Autowired
+	private final CompraRepository compraRepository;
+	@Autowired
+	private final TarjetaRepository tarjetaRepository;
+	@Autowired
+	private final ClienteRepository clienteRepository;
+	@Autowired
+	private final UserRepository userRepository;
 
-    public CompraService(CompraRepository compraRepository, TarjetaRepository tarjetaRepository,
-                         ClienteRepository clienteRepository, UserRepository userRepository) {
-        this.compraRepository = compraRepository;
-        this.tarjetaRepository = tarjetaRepository;
-        this.clienteRepository = clienteRepository;
-        this.userRepository = userRepository;
-    }
+	public CompraService(CompraRepository compraRepository, TarjetaRepository tarjetaRepository,
+			ClienteRepository clienteRepository, UserRepository userRepository) {
+		this.compraRepository = compraRepository;
+		this.tarjetaRepository = tarjetaRepository;
+		this.clienteRepository = clienteRepository;
+		this.userRepository = userRepository;
+	}
 
-<<<<<<< Updated upstream
-    public ResponseEntity<String> comprar(CompraDTO compraDTO) throws IOException {
-        if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
-            if (compraDTO.getToken() == null || compraDTO.getPrecio() == null)
-                throw new BadRequestAlertException("falta token y/o monto", "tarjeta", "missing parameters");
-            if (tarjetaRepository.findByToken(compraDTO.getToken()).getCliente().getUser().getId() != userRepository
-                .findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId())
-                throw new BadRequestAlertException("No te pertenece ese cliente", "tarjeta", "prohibido");
-            if (tarjetaRepository.findByTokenOpt(compraDTO.getToken()).get().isAlta() != true) {
-                throw new BadRequestAlertException("La Tarjeta esta dada de baja", "tarjeta", "prohibido");
-            }
-        }
-        TarjetaDTO tarjetaDTO = new TarjetaDTO(compraDTO.getToken(), compraDTO.getPrecio());
-        HttpResponse verificacionTarjeta = PostUtil.sendPost(tarjetaDTO.toString(),
-            "http://127.0.0.1:8081/api/tarjeta/tarjeta");
-        HttpResponse verificacionMonto = PostUtil.sendPost(tarjetaDTO.toString(),
-            "http://127.0.0.1:8081/api/tarjeta/monto");
-        HttpResponse response = PostUtil.sendPost(tarjetaDTO.toString(), "http://127.0.0.1:8081/api/tarjeta/comprar");
-        // No me gusta este metodo de agarrar si mando un 200 (buscar alternativa)
+	public ResponseEntity<String> comprar(CompraDTO compraDTO) throws IOException {
+		// if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+		if (compraDTO.getToken() == null || compraDTO.getPrecio() == null)
+			throw new BadRequestAlertException("falta token y/o monto", "tarjeta", "missing parameters");
+		if (tarjetaRepository.findByToken(compraDTO.getToken()).getCliente().getUser().getId() != userRepository
+				.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get().getId())
+			throw new BadRequestAlertException("No te pertenece ese cliente", "tarjeta", "prohibido");
+		if (tarjetaRepository.findByTokenOpt(compraDTO.getToken()).get().isAlta() != true) {
+			throw new BadRequestAlertException("La Tarjeta esta dada de baja", "tarjeta", "prohibido");
+		}
 
-        Compra compra = new Compra();
-        compra.setCliente(tarjetaRepository.findByToken(compraDTO.getToken()).getCliente());
-        compra.setDescripcion(compraDTO.getDescripcion());
-        compra.setPrecio(compraDTO.getPrecio());
-        compra.setUser(userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().get()).get());
-        compra.setTarjeta(tarjetaRepository.findByToken(compraDTO.getToken()));
-        if (response.getStatusLine().getStatusCode() == 201) {
-            compra.setValido(true);
-            Compra result = compraRepository.save(compra);
-            return new ResponseEntity<String>(EntityUtils.toString(response.getEntity(), "UTF-8"), HttpStatus.OK);
-        } else {
-            compra.setValido(false);
-            Compra result = compraRepository.save(compra);
-            return new ResponseEntity<String>(EntityUtils.toString(response.getEntity(), "UTF-8"),
-                HttpStatus.FORBIDDEN);
-        }
-    }
-=======
 		// }
 		TarjetaDTO tarjetaDTO = new TarjetaDTO(compraDTO.getToken(), compraDTO.getPrecio());
 
@@ -121,14 +101,13 @@ public class CompraService {
 			return ResponseEntity.ok().headers(headers).body(result.toString());
 		}
 	}
->>>>>>> Stashed changes
 
-    public ResponseEntity<List<Compra>> findAllByUserId() throws IOException {
-        List<Compra> list = compraRepository.findAllByUserId();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "*/*");
-        return ResponseEntity.ok().headers(headers).body(list);
-    }
+	public ResponseEntity<List<Compra>> findAllByUserId() throws IOException {
+		List<Compra> list = compraRepository.findAllByUserId();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", "*/*");
+		return ResponseEntity.ok().headers(headers).body(list);
+	}
 
 }
 
